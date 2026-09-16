f62afc03ced77487e6ed6c1f1f1c39bdab3e4292
Jacob Cormier <jacobcormier02@gmail.com>
2026-09-16 15:10:43 -0400

    Update for loading sardet100k dataset

--- dataset.py
+++ dataset.py
@@ -94,3 +94,23 @@ class SARDet100KDataset(torch.utils.data.Dataset):
 
 def collate_fn(batch):
     return tuple(zip(*batch))
+
+class SARDet100KDatasetSSLTraing(torch.utils.data.Dataset):
+    def __init__(self, image_dir, transforms=None):
+        self.image_dir = image_dir
+        self.transform = self.transform
+        self.images = [file for file in os.listdir(image_dir)]
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image_name = self.images[idx]
+        image_path = os.path.join(self.image_dir, self.images[idx])
+
+        image = Image.open(image_path)
+
+        if self.transforms is not None:
+            image, target = self.transforms(image, target)
+
+        return image
--- train_swin.sh
+++ train_swin.sh
@@ -3,10 +3,9 @@ python main_dino.py \
     --arch swin_tiny \
     --patch_size 4 \
     --window_size 7 \
-    --data_path datasets/imagenette2-160/train \
-    --batch_size_per_gpu 40 \
+    --data_path datasets/SARDet_100K/JPEGImages/val \
+    --batch_size_per_gpu 2 \
     --epochs 100 \
     --use_dense_prediction True \
     --use_lora True \
     --pretrained_path swin_tiny_esvit.pth
-    # --use_lora True --pretrained_path swin_tiny_patch4_window7_224.pth
